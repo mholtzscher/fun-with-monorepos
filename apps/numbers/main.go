75df6ca54037e2615a226680a75dc184d12a0eca
Michael Holtzscher <thenerdproject1@gmail.com>
2023-04-15 10:39:26 -0500

    fix: handle errors from gin Run invocations

--- apps/numbers/main.go
+++ apps/numbers/main.go
@@ -20,5 +20,8 @@ func main() {
 			"result": math.Factorial(num),
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080
 }
--- apps/ping/main.go
+++ apps/ping/main.go
@@ -9,5 +9,8 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080
 }
