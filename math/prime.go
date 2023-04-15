4f76433dd74c00597c1e889757202722b347c302
Michael Holtzscher <thenerdproject1@gmail.com>
2023-04-15 11:24:25 -0500

    feat(math): add is_prime func to math package

---
+++ math/prime.go
@@ -0,0 +1,17 @@
+package math
+
+import "math"
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ math/prime_test.go
@@ -0,0 +1,52 @@
+package math
+
+import (
+	"testing"
+)
+
+func TestIsPrime(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    int
+		expected bool
+	}{
+		{"One", 1, false},
+		{"Two", 2, true},
+		{"Three", 3, true},
+		{"Four", 4, false},
+		{"Five", 5, true},
+		{"Six", 6, false},
+		{"Seven", 7, true},
+		{"Eight", 8, false},
+		{"Nine", 9, false},
+		{"Ten", 10, false},
+		{"Eleven", 11, true},
+		{"Twelve", 12, false},
+		{"Thirteen", 13, true},
+		{"Fourteen", 14, false},
+		{"Fifteen", 15, false},
+		{"Sixteen", 16, false},
+		{"Seventeen", 17, true},
+		{"Eighteen", 18, false},
+		{"Nineteen", 19, true},
+		{"Twenty", 20, false},
+		{"Twenty-one", 21, false},
+		{"Twenty-two", 22, false},
+		{"Twenty-three", 23, true},
+		{"Twenty-four", 24, false},
+		{"Twenty-five", 25, false},
+		{"Twenty-six", 26, false},
+		{"Twenty-seven", 27, false},
+		{"Twenty-eight", 28, false},
+		{"Twenty-nine", 29, true},
+		{"Thirty", 30, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if isPrime(tt.input) != tt.expected {
+				t.Errorf("isPrime(%d) = %v; want %v", tt.input, !tt.expected, tt.expected)
+			}
+		})
+	}
+}
